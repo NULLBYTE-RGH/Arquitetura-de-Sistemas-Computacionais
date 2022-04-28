bdfb3209b990888679228b48ecb5ebb22a69586e
NULLBYTE-RGH <79452652+NULLBYTE-RGH@users.noreply.github.com>
2022-04-28 19:42:36 -0300

    Add files via upload

--- microServiço1.js
+++ microServiço1.js
@@ -1,32 +1,61 @@
 const got = require('got');
-const jsdom = require("jsdom");
 const fs = require('fs');
 const { url } = require('inspector');
+const scrape = require('website-scraper');
+const PuppeteerPlugin = require('website-scraper-puppeteer');
+const path = require('path');
+const { PassThrough } = require('stream');
+
 require('dotenv').config()
 
-const { JSDOM } = jsdom;
+
 
 let sites = process.env.URLS.split(",");
 const tamanho = sites.length
 
-for (let i = 0;i<tamanho;i++){
-    const Url= sites[i]
+var hora = new Date();
+var dia = new Date();
 
-const Filtro1 = (link) => {
-  return link;
-};
 
-(async () => {
-  const response = await got(Url);
-  var Nome = Url.toString().split("/")[2]
+for (let i = 0;i < tamanho;i++) {
 
-  Nome = `(${Nome}).html`
-  console.log(Nome)
+const Url= sites[i]
+let Nome = Url.toString().split("/")[2]
+console.log(`Clonando ${Url}.....`)
 
-fs.writeFile(Nome, response.body, (err) => {
+try{
+scrape({
+  
+  urls: [Url],
+  directory: path.resolve(__dirname, Nome),
+
+  plugins: [ 
+      new PuppeteerPlugin({
+          launchOptions: { 
+              
+              headless: true
+          }, 
+          scrollToBottom: {
+              timeout: 30000, 
+              viewportN: 54321 
+          }
+      })
+  ]
+},
+
+()=>{
+
+var hora = hora.getHours() + ":" + hora.getMinutes() + ":" + hora.getSeconds();
+var dia = dia.getFullYear()+'-'+(dia.getMonth()+1)+'-'+dia.getDate();
+let data = {hora,dia}
+data = JSON.stringify(data);
+
+fs.writeFile(`${Nome}/DATAclonagem.json`, data, (err) => {
     if (err) throw err;
-  console.log(`Site ${Url} Clonado com sucesso!`);
+  console.log(`Clonagem Completa! ---> ${Nome}`);
 
-});
-})()
+})})
+
+}
+catch(e){}
 }
