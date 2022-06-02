a538246c12248fa73aa517af35cf08acdc491490
NULLBYTE-RGH <79452652+NULLBYTE-RGH@users.noreply.github.com>
2022-06-01 23:52:26 -0300

    Add files via upload

--- Angular/projeto1/src/app/componente-criar-conta-cli/componente-criar-conta-cli.component.ts
+++ Angular/projeto1/src/app/componente-criar-conta-cli/componente-criar-conta-cli.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Cadastro } from '../Cadastro';
+import { ServicoLoginService } from '../serviços/servico-login.service';
 @Component({
   selector: 'app-componente-criar-conta-cli',
   templateUrl: './componente-criar-conta-cli.component.html',
   styleUrls: ['./componente-criar-conta-cli.component.css']
 })
 export class ComponenteCriarContaCliComponent implements OnInit {
+  Vazio:boolean=false;;
+  PrimeiroNome = ''
+  UltimoNome = ''
+  Username = ''
+  Email = ''
+  Senha = ''
+  Pais = ''
 
-  constructor() { }
+  Cadastro!:Cadastro
+
+  constructor(private servicoCadastro:ServicoLoginService) { }
 
   ngOnInit(): void {
   }
 
+  Validar(PrimeiroNome:String,UltimoNome:String,Username:String,Email:String,Senha:String,Pais:String){
+
+    if ((PrimeiroNome && UltimoNome && Username && Email && Senha && Pais) == ''){
+      this.Vazio=true
+      }
+      else{
+        this.Vazio=false
+        this.Cadastro = {PrimeiroNome:PrimeiroNome,UltimoNome:UltimoNome,Username:Username,Email:Email,Senha:Senha,Pais:Pais}
+        this.Cadastrar(this.Cadastro)
+        
+      }
+  }
+
+  Cadastrar(dados:Cadastro){
+    this.servicoCadastro.cadastrar(dados)
+  }
 }
--- Angular/projeto1/src/app/componente-login-cli/componente-login-cli.component.ts
+++ Angular/projeto1/src/app/componente-login-cli/componente-login-cli.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../Login';
+import { ServicoLoginService } from '../serviços/servico-login.service';
 
 @Component({
   selector: 'app-componente-login-cli',
@@ -6,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./componente-login-cli.component.css']
 })
 export class ComponenteLoginCliComponent implements OnInit {
+  Email='';
+  Senha='';
+  Vazio:boolean=false;;
 
-  constructor() { }
+  Login!:Login;
+
+  Enviar(Email:string,Senha:string){
+    if ((Email && Senha) == ''){
+    this.Vazio=true
+    }
+    else{
+      this.Vazio=false
+      this.Login ={Email:Email,Senha:Senha}
+      this.Logar(this.Login)
+    }
+  }
+
+  Logar(dados:Login){
+    this.servicoLogin.logar(dados)
+  }
+
+  constructor(private servicoLogin:ServicoLoginService) { }
 
   ngOnInit(): void {
   }
